296a870c2db1aa63310f1a3d6a9f4abd9d158164
VermilionB <serezha.mostyko@mail.ru>
2024-01-18 12:02:24 +0300

    counter kanban added

--- src/pages/DropdownPage.jsx
+++ src/pages/DropdownPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Accordion, Grid, Title} from "@mantine/core";
+import { Accordion, Grid, Title } from "@mantine/core";
 import statuses from "../data/statuses.json";
 import data from "../data/dropdownPageData.json";
 import CanbanTask from "../components/CanbanTask";
@@ -8,18 +8,24 @@ const DropdownPage = () => {
     return (
         <>
             <Grid columns={3} mt="50px" mx="300px" justify="center">
-                {statuses.map(item => (
-                    <Grid.Col span={1} key={item.id} className="canbanColumn">
-                        <Title order={3} mb="20px">{item.status}</Title>
-                        <Accordion variant="separated" multiple>
-                            {data.map(task => (
-                                <>
-                                    {task.category === item.id && <CanbanTask data={task}/>}
-                                </>
-                            ))}
-                        </Accordion>
-                    </Grid.Col>
-                ))}
+                {statuses.map(item => {
+                    const taskCount = data.filter(task => task.category === item.id).length;
+
+                    return (
+                        <Grid.Col span={1} key={item.id} className="canbanColumn">
+                            <Title order={3} mb="20px">
+                                {item.status} ({taskCount})
+                            </Title>
+                            <Accordion variant="separated" multiple>
+                                {data.map(task => (
+                                    <>
+                                        {task.category === item.id && <CanbanTask data={task} />}
+                                    </>
+                                ))}
+                            </Accordion>
+                        </Grid.Col>
+                    );
+                })}
             </Grid>
         </>
     );
